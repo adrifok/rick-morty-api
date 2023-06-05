2059606105910ac33048c0b75112659e2939408d
adrifok <adrifok@gmail.com>
2023-06-05 17:10:48 -0500

    dockerfile added

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Use the official Node.js 14 image as the base image
+FROM node:14
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the package.json and package-lock.json files to the container
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the app's source code to the container
+COPY . .
+
+# Build the app
+RUN npm run build
+
+# Expose a port (if your app needs it)
+EXPOSE 3000
+
+# Set the command to run your app using npm
+CMD [ "npm", "start" ]
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -8,16 +8,17 @@ export default function Cards(props) {
    return (
       <div className={styles.cards}>
       {characters.map(character => (
-      <Card 
-      detailId={character.id}
-      key={character.id}
-      name={character.name} 
-      gender={character.gender}
-      species={character.species}
-      image={character.image}
-      onClose={() => props.onClose(character.id)} 
-      />
-      ))}
+         <Card 
+         detailId={character.id}
+         key={character.id}
+         name={character.name} 
+         gender={character.gender}
+         image={character.image}
+         species={character.species}
+         onClose={() => props.onClose(character.id)} 
+         />
+         ))}
+         <h3 className={styles.love}>Made with &hearts; by Adriana Ferrari</h3>
    </div>
    );
 }
